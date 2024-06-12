d9cd18258292edeccd2056ebf5d3ae899198a7d4
est3banst <estebeeen@outlook.com>
2024-06-12 15:29:54 -0300

    se agrega un poco de css

--- proyecto/main.js
+++ proyecto/main.js
@@ -7,19 +7,25 @@ async function displayProds() {
     const prodS = await response.json();
     
     const productList = prodS.products   
+    console.log(productList);
     let elementsHolder = document.getElementById('product-container');
     let data = ''
     for (let product of productList) {
         data += `
-               <div class='div-for-product'>
+               <section class='div-for-product'>
                <figure>
                <img src=${product.images[0]}
                alt=${product.category}>
                </figure>
-               <figcaption>${product.description} </figcaption>
-                 <p>${product.title}</p>
-                 <span>\$${product.price}</span>
+               <figcaption><h2>${product.title}</h2></figcaption>
+               <hr/>
+                 <p>${product.description}</p>
+                 <div class='div-buy-product'>
+                 <span class='span-old-price'>\$${Math.round(product.price * 1.20)}</span>
+                 <span class='span-price'>\$${product.price}</span>
+                 <span class='span-action'>Agregar al carrito</span>
                  </div>
+                 </section>
                `
     } elementsHolder.innerHTML = data
 }
