bfe279b8fdc1f8684535c174d4fa7bc014b3bbf3
est3banst <estebeeen@outlook.com>
2024-06-18 21:56:26 -0300

    se agregan estilos

--- proyecto/main.js
+++ proyecto/main.js
@@ -7,7 +7,7 @@ async function displayProds() {
     const prodS = await response.json();
     
     const productList = prodS.products   
-    console.log(productList);
+    console.log(productList[1]);
     let elementsHolder = document.getElementById('product-container');
     let data = ''
     for (let product of productList) {
@@ -15,15 +15,16 @@ async function displayProds() {
                <section class='div-for-product'>
                <figure>
                <img src=${product.images[0]}
+               class='img-product'
                alt=${product.category}>
                </figure>
                <figcaption><h2>${product.title}</h2></figcaption>
                <hr/>
-                 <p>${product.description}</p>
+                 <p class='product-descr'>${product.description}</p>
                  <div class='div-buy-product'>
                  <span class='span-old-price'>\$${Math.round(product.price * 1.20)}</span>
                  <span class='span-price'>\$${product.price}</span>
-                 <span class='span-action'>Agregar al carrito</span>
+                 <a class='button-action'>Agregar al carrito</a>
                  </div>
                  </section>
                `
