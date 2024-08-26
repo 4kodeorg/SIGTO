f88b74211bf98702a181d1e7fab06be9adbffa2a
est3banst <estebeeen@outlook.com>
2024-08-26 11:55:58 -0300

    agregado a gitignore

--- proyecto/config/db.php
+++ proyecto/config/db.php
@@ -2,24 +2,22 @@
 
 class Database {
     
-    private $host = "localhost:3306";
+    private $host = "127.0.0.1:3306";
     private $db_name = "dbtest";
     private $user = "superman";
     private $password = "test";
     private $conn;
 
 
-    public function __construct()
+    public function getConnection()
     {
-        if(!isset($this->conn)) {
-            try { $this->conn = new mysqli($this->host,$this->user, $this->password, $this->db_name); }
+        $this->conn = null;
+            try { 
+                $this->conn = new mysqli(
+                    $this->host,$this->user, $this->password, $this->db_name); }
             catch (Exception $e) {
                 echo "Error al conectar con la base de datos". $e->getMessage();
             }
-        }
+            return $this->conn;
     }
-    public function getConnection() {
-        return $this->conn;
-    }
-    
 }
