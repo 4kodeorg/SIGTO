6a5fce6f510c9398300b4231847e7aa809f3c760
PabloGilberti <pgilberti@gmail.com>
2024-09-06 20:44:42 -0300

    Se modifica el head del index, logo y colores, se modifica favicon

--- proyecto/header.php
+++ proyecto/header.php
@@ -16,9 +16,9 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
+    <link rel="icon" type="image/png" href="assets/imgs/favicon.jpeg"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>youCommerce</title>
+    <title>!Mercado Ya¡</title>
 </head>
 <body>
 
@@ -26,7 +26,7 @@
     <div class="container-fluid px-4" >
        <div class="brand-container">
        <a href="" class="navbar-brand">
-            <h1 class="logo">Mercado Ya</h1>
+       <img src="assets/imgs/Mercado_ya.png" alt="Logo" width="200" height="80" class="d-inline-block align-text-top">
         </a>
             <form class="d-flex searchbar2" role="search" action="" method="post">
                 <label for="busqueda"></label>
