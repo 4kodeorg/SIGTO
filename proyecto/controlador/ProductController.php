e18e9d608d0a9c14224d24751b5040904478d1fb
est3banst <estebeeen@outlook.com>
2024-10-15 15:26:42 -0300

    metodos en controladores

--- proyecto/controlador/ProductController.php
+++ proyecto/controlador/ProductController.php
@@ -1,9 +1,8 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'] . '/modelo/Producto.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/config/Database.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/modelo/Producto.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/config/Database.php';
 
-class ProductController extends Database
-{
+class ProductController extends Database {
     public function create($data)
     {
         $producto = new Producto();
@@ -22,20 +21,15 @@ class ProductController extends Database
     {
         $query = 'INSERT INTO productos (titulo, descripcion, origen, cantidad, precio) VALUES (?, ?, ?, ?, ?);';
         $stmt = $this->conn->prepare($query);
-
+      
         $titulo = $producto->getTitulo();
         $descripcion = $producto->getDescripcion();
         $origen = $producto->getOrigen();
         $cantidad = $producto->getCantidad();
         $precio = $producto->getPrecio();
-
+    
         $stmt->bind_param(
-            'sssss',
-            $titulo,
-            $descripcion,
-            $origen,
-            $cantidad,
-            $precio
+            'sssss', $titulo, $descripcion, $origen, $cantidad, $precio     
         );
         error_log("Error: " . $stmt->error);
         if ($stmt->execute()) {
@@ -45,151 +39,9 @@ class ProductController extends Database
         }
     }
 
-    public function getProductsByLimit($offset = 0, $limit = 15)
-    {
-        $query = 'SELECT * FROM productos WHERE estado=1 LIMIT ?, ?;';
-        $stmt = $this->conn->prepare($query);
-        $stmt->bind_param('ii', $offset, $limit);
-        try {
-            if ($stmt->execute()) {
-                $result = $stmt->get_result();
-                return $result->fetch_all(MYSQLI_ASSOC);
-            }
-        } catch (Exception $err) {
-            echo "Error en la base de datos" . $err->getMessage();
-        }
+    public function getFirstTenProducts() {
+        $query = 'SELECT * FROM productos LIMIT 10;';
     }
 
-    public function searchProductsByTitleOrDescripcion($searchTerm)
-    {
-        $query = 'SELECT * FROM productos WHERE estado=1 AND (titulo LIKE ? OR descripcion LIKE ?);';
-        $stmt = $this->conn->prepare($query);
-        if (!$stmt) {
-            throw new Exception("Error al buscar");
-        }
-        $searchParamComodines = '%' . $searchTerm . '%';
-        $stmt->bind_param('ss', $searchParamComodines, $searchParamComodines);
-        $stmt->execute();
-
-        $result = $stmt->get_result();
-        $productos = $result->fetch_all(MYSQLI_ASSOC);
-        $stmt->close();
-        return $productos ?: [];
-    }
-    public function getProductById($productId)
-    {
-        $query = "SELECT * FROM productos WHERE estado=1 AND id= ?;";
-        $stmt = $this->conn->prepare($query);
-        $stmt->bind_param('i', $productId);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $producto = $result->fetch_assoc();
-        $stmt->close();
-        return $producto;
-    }
-    public function getDisabledProducts()
-    {
-        $query = "SELECT * FROM productos WHERE estado=0;";
-        $stmt = $this->conn->prepare($query);
-        if (!$stmt) {
-            throw new Exception("Error al traer los productos desactivados");
-        }
-        if ($stmt->execute()) {
-            $result = $stmt->get_result();
-            $productos = $result->fetch_all(MYSQLI_ASSOC);
-        } 
-        $stmt->close();
-        return $productos ?? [];
-
-    }
-
-    public function updateProductData($data)
-    {
-        $query = "UPDATE productos SET titulo=?, descripcion=?, origen=?, cantidad=?, precio=? WHERE id=?;";
-        $stmt = $this->conn->prepare($query);
-
-        $stmt->bind_param(
-            'sssssi',
-            $data['new_titulo'],
-            $data['new_descripcion'],
-            $data['new_origen'],
-            $data['new_cantidad'],
-            $data['new_precio'],
-            $data['product_id']
-        );
-        try {
-            if ($stmt->execute()) {
-                return true;
-            } else {
-                throw new Exception("Error al actualizar");
-            }
-        } catch (Exception $err) {
-            throw new Exception('Error en la base de datos: ' . $err->getMessage());
-        }
-        $stmt->close();
-    }
-    public function deleteProductById($productId)
-    {
-        $this->conn->begin_transaction();
-
-        try {
-            $queryCarrito = "DELETE FROM carrito WHERE id_prod = ?";
-            $stmtCarrito = $this->conn->prepare($queryCarrito);
-            $stmtCarrito->bind_param('i', $productId);
-
-            if (!$stmtCarrito->execute()) {
-                throw new Exception("Error en carrito: " . $stmtCarrito->error);
-            }
-
-            $queryProductos = "DELETE FROM productos WHERE id = ?";
-            $stmtProductos = $this->conn->prepare($queryProductos);
-            $stmtProductos->bind_param('i', $productId);
-
-            if (!$stmtProductos->execute()) {
-                throw new Exception("Error en productos: " . $stmtProductos->error);
-            }
-            $this->conn->commit();
-
-            return true;
-        } catch (Exception $e) {
-            $this->conn->rollback();
-            error_log("Error: " . $e->getMessage());
-            return false;
-        } finally {
-            $stmtCarrito->close();
-            $stmtProductos->close();
-        }
-    }
-    public function activateProduct($productId) {
-        $query = "UPDATE productos set estado=1 where id=?;";
-        $stmt = $this->conn->prepare($query);
-        if (!$stmt) {
-            throw new Exception("Error al intentar activar el producto");
-        }
-        $stmt->bind_param('i', $productId);
-        try {
-            if ($stmt->execute()) {
-                return true;
-            }
-        } catch (Exception $err) {
-            throw new Exception("Error" .$err->getMessage());
-        }
-    }
-
-    public function disableProductById($productId)
-    {
-        $query = "UPDATE productos set estado=0 where id=?;";
-        $stmt = $this->conn->prepare($query);
-        if (!$stmt) {
-            throw new Exception("Error al intentar desactivar el producto");
-        }
-        $stmt->bind_param('i', $productId);
-        try {
-            if ($stmt->execute()) {
-                return true;
-            }
-        } catch (Exception $e) {
-            throw new Exception("Error: " . $e->getMessage());
-        }
-    }
 }
+
--- proyecto/controlador/UsuarioController.php
+++ proyecto/controlador/UsuarioController.php
@@ -24,7 +24,7 @@ class UsuarioController extends Database
     }
     public function createUser($usuario)
     {
-        $query = "INSERT INTO usuarios (name, lastname, email, username, passw, telefono, fecha_nac, direccion) VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
+        $query = 'INSERT INTO usuarios (name, lastname, email, username, passw, telefono, fecha_nac, direccion) VALUES (?, ?, ?, ?, ?, ?, ?, ?);';
         $stmt = $this->conn->prepare($query);
 
         $name = $usuario->getNombre();
@@ -41,7 +41,7 @@ class UsuarioController extends Database
         );
         error_log("Error: " . $stmt->error);
         if ($stmt->execute()) {
-            $query = "SELECT * FROM usuarios WHERE id=".$this->conn->insert_id.";";
+            $query = 'SELECT * FROM usuarios WHERE id='.$this->conn->insert_id.';';
             $stmtn = $this->conn->prepare($query);
             if($stmtn->execute()) {
                 $res = $stmtn->get_result();
@@ -53,42 +53,6 @@ class UsuarioController extends Database
         }
     }
     }
-    public function checkGoogleUser ($email) {
-        $query = "SELECT email FROM google_cuentas WHERE email= ?;";
-        $stmt = $this->conn->prepare($query);
-        if (!$stmt) {
-            throw new Exception("Error al crear la cuenta");
-        }
-        $stmt->bind_param('s', $email);
-        try {
-            if ($stmt->execute()) {
-                $result = $stmt->get_result();
-                if ($result->num_rows == 1) {
-                    $googleUser = $result->fetch_assoc();
-                    
-                    return $googleUser;
-                }
-                else {
-                    return false;
-                }
-            }
-        } catch (Exception $er) {
-            throw new Exception("Error: " .$er->getMessage());
-        }
-    }
-    public function createGoogleUser($email, $name) {
-        $query = "INSERT INTO google_cuentas (email, fullname) VALUES ( ?, ?);";
-        $stmt = $this->conn->prepare($query);
-        if (!$stmt) {
-            throw new Exception("Error al crear la cuenta");
-        }
-        $stmt->bind_param('ss', $email, $name);
-        try {
-            return $stmt->execute();
-        } catch (Exception $err) {
-            throw new Exception("Error: ".$err->getMessage());
-        }
-    }
     public function validateUser($username, $password) {
         
         $query = 'SELECT * FROM usuarios WHERE username=?';
@@ -97,16 +61,14 @@ class UsuarioController extends Database
         if (!$stmt) {
             throw new Exception("Error en la base de datos");
         }
-        $paramUsername = $username;
         $stmt->bind_param('s', $paramUsername);
+        
+        $paramUsername = $username;
 
         if($stmt->execute()) {
             $res = $stmt->get_result();
             if( $res->num_rows == 1 ) {
                 $usuario = $res->fetch_assoc();
-                if (!$usuario) {
-                    return false;
-                }
                 $passwd = $usuario['passw'];
                 if (password_verify($password, $passwd)) {
                     return $usuario;
@@ -123,102 +85,4 @@ class UsuarioController extends Database
         }
         
     }
-    public function updateUserDireccion($idUser, $direccionEnvio, $segDireccionEnvio) {
-        $query = "UPDATE usuarios SET direccion =?, seg_direccion= ? WHERE id=? ;";
-        $stmt = $this->conn->prepare($query);
-        $stmt->bind_param('ssi',$direccionEnvio,$segDireccionEnvio, $idUser);
-        if ($stmt->execute()) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    public function getUserProductFavs($userId) {
-        $query = "
-            SELECT productos.* 
-            FROM productos
-            INNER JOIN favoritos ON productos.id = favoritos.id_prod
-            WHERE favoritos.id_user = ?;
-        ";
-        
-        $stmt = $this->conn->prepare($query);
-        $stmt->bind_param("i", $userId); 
-    
-        $stmt->execute();
-        $result = $stmt->get_result();
-    
-        return $result->fetch_all(MYSQLI_ASSOC);
-    }
-    
-    public function updateUserData($idUser, $newEmail, $newPhone, $newUser) {
-        $query = "UPDATE usuarios SET email=?, username=?, telefono=? WHERE id=?;";
-        $stmt = $this->conn->prepare($query);
-        $stmt->bind_param('ssii', $newEmail, $newUser, $newPhone, $idUser);
-        if ($stmt->execute()) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public function getUserbyId($idUser) {
-        $query = "SELECT * FROM usuarios WHERE id = ?;";
-        $stmt = $this->conn->prepare($query);
-        $stmt->bind_param('i', $idUser);
-         if ($stmt->execute()) {
-            $result = $stmt->get_result();
-            $usuario = $result->fetch_assoc();
-            $stmt->close();
-            return $usuario;
-         }
-        else {
-            throw new Exception("Error en la base datos");
-            }
-        }
-
-    public function addToFav($idUser, $idProd) {
-        $query = "INSERT into favoritos (id_user, id_prod) VALUES (?, ?);";
-        $stmt = $this->conn->prepare($query);
-        $stmt->bind_param('ii', $idUser, $idProd);
-        if ($stmt->execute()) {
-            return true;
-        } else {
-            return false;
-        }
-
-    }
-
-    public function getUserFavorites ($idUser) {
-        $query = "SELECT * FROM favoritos WHERE id_user=?;";
-        $stmt = $this->conn->prepare($query);
-        $stmt->bind_param('i', $idUser);
-        if ($stmt->execute()) {
-            $result = $stmt->get_result();
-            if ($result->num_rows > 0) {
-            $favoritos = $result->fetch_all(MYSQLI_ASSOC); 
-            }
-            else {
-                $favoritos = [];
-            }
-            $stmt->close();
-            return $favoritos;
-        }
-        else {
-            throw new Exception("Error en la base de datos");
-        }
-    }
-    public function deleteFromFavorites ($idUser, $idProd) {
-        $query = "DELETE FROM favoritos WHERE id_user=? AND id_prod=?;";
-        $stmt = $this->conn->prepare($query);
-        $stmt->bind_param('ii', $idUser, $idProd);
-        if ($stmt->execute()) {
-            $stmt->close();
-            return true;
-        }
-        else {
-            $stmt->close();
-            return false;
-        }
-    }
-
 }
